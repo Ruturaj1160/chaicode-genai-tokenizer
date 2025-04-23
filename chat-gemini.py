931c059e8f07218c93a8eff26fe80f83e984d8ec
Ruturaj B Jadhav <ruturajb.jadhav@kochcc.com>
2025-04-24 00:20:20 +0530

    add system prompt

--- chat-gemini.py
+++ chat-gemini.py
@@ -7,15 +7,42 @@ load_dotenv()
 
 try:
     client = OpenAI(
-    api_key = os.getenv("GEMINI_API_KEY"), 
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
+        api_key=os.getenv("GEMINI_API_KEY"),
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+
+    )
 
     system_prompt = """
-    You are helpfull
+    You are helpful AI assisttan who is specailised in resolving user query.
+    You work on start, plan, action and observe mode.
+    For the given user query and available tools, plan the step by step execution, based on planning, select the releavnt tool and 
+    based on the tool selection you perform an action and action to call the tool. Wait for observation and based on the observation you will decide the next step.
+
+    Rules:
+    - Follow the output JSON format strictly.
+    - Always perforom one step at a time and wait for next input
+    - Carefully analyze the user query and plan the steps.
+
+    Output JSON format:
+    {{
+    "Step": "string",
+    "content": "string",
+    "function": "The name of function if the step is action",
+    "input": "The input to the function if the step is action",
+    }}
+    
+    Example:
+    User Query: What is the weather of New York?
+    Output: {{ "step": "plan", "content":"The user is interested in weather data of new york."}}
+    Output: {{ "step":"plan", "content":"I will use the weather API to get the weather data of new york."}}
+    Output: {{ "step":"action", "function":"get_weather", "input":"New York"}}
+    Output: {{ "step":"observe", "output": "12 degree celsius, sunny"}}
+    Output:{{"output":"The weather of New York is 12 degree celsius and sunny."}}
+
     """
 
     reponse = client.chat.completions.create(
-        model="gemini-2.0-flash", # or "gemini-1.5-turbo-16k"
+        model="gemini-2.0-flash",  # or "gemini-1.5-turbo-16k"
         messages=[
             {
                 "role": "user",
